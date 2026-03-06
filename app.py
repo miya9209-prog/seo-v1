af32a4e606417f9074e182891d24a4fd0dffc8e3
miya9209-prog <miya9209@gmail.com>
2026-03-07 03:48:30 +0900

    Fix indentation for render_usage_tips function call

--- app.py
+++ app.py
@@ -731,7 +731,7 @@ def main() -> None:
 
 
     with st.expander("사용팁", expanded=False):
-    render_usage_tips()
+        render_usage_tips()
 
     default_url = "https://www.misharp.co.kr/product/detail.html?product_no=28522&cate_no=24&display_group=1"
     url = st.text_input(
